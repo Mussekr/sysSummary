e1c98cd01e5e03f7aa9bef837c8a5b553197715b
Rasmus Kröger <rasmuskroger@gmail.com>
2018-10-13 18:42:15 +0300

    Remove process promises

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import { getAllData, cpuTemperature } from 'systeminformation';
-import { exec } from 'process-promises';
-
+import * as childProcess from 'child_process';
 getAllData().then((data) => console.log(data)).catch((err) => console.error(err));
-
-exec('/var/local/kuhler_ctl -l')
-    .on('process', (process) => console.log('Pid: ', process.pid))
-    .then((result) => {
-        console.log('stdout: ', result.stdout);
-        console.log('stderr: ', result.stderr);
-    })
-    .fail((err) => {
-        console.error('ERROR: ', err);
-    });
-
+childProcess.exec('/var/local/kuhler_ctl -l', (err, stdout, stderr) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(stdout);
+    console.log(stderr);
+});
 // cpuTemperature().then((data) => console.log(data)).catch((err) => console.error(err));
