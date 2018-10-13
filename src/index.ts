d39246313743b27955215a12b6c688bc671aef5e
Rasmus Kröger <rasmuskroger@gmail.com>
2018-10-13 18:34:02 +0300

    Add kuhler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { getAllData, cpuTemperature } from 'systeminformation';
+import { exec } from 'ts-process-promises';
 
 getAllData().then((data) => console.log(data)).catch((err) => console.error(err));
+
+exec('/var/local/kuhler_ctl -l')
+    .on('process', (process) => console.log('Pid: ', process.pid))
+    .then((result) => {
+        console.log('stdout: ', result.stdout);
+        console.log('stderr: ', result.stderr);
+    })
+    .fail((err) => {
+        console.error('ERROR: ', err);
+    });
 
 // cpuTemperature().then((data) => console.log(data)).catch((err) => console.error(err));
